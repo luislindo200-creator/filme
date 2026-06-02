3112c4c186e4a33fef6e4fe7409456e99301fdbf
Felipe Photos <felipephotositz@gmail.com>
2026-06-02 13:26:55 -0300

    feat: suporte a PWA (Progressive Web App) adicionado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Voyo',
+        short_name: 'Voyo',
+        description: 'Seu guia universal de streaming',
+        theme_color: '#141414',
+        background_color: '#141414',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
