fdeb8d7f34e7d10bcd565328f4dc4d21a661f298
Felipe Photos <felipephotositz@gmail.com>
2026-06-02 13:22:39 -0300

    fix: corrige caminhos das imagens dos provedores de streaming

--- src/components/ProviderBar.jsx
+++ src/components/ProviderBar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './ProviderBar.css';
 
 const providers = [
-  { id: 8, name: 'Netflix', logo: 'https://image.tmdb.org/t/p/original/t2yyOv40HZeVlLjVrCsPhqZf4O1.jpg' },
-  { id: 119, name: 'Prime Video', logo: 'https://image.tmdb.org/t/p/original/emthp39XA2YScoYL1p0sfbEKI4x.jpg' },
-  { id: 337, name: 'Disney+', logo: 'https://image.tmdb.org/t/p/original/7rwgEs15tFwyR9NPQ5arvixRVxG.jpg' },
-  { id: 1899, name: 'Max', logo: 'https://image.tmdb.org/t/p/original/6UhBX2A3tPrc7zMIsL1P1bX5y9a.jpg' },
-  { id: 350, name: 'Apple TV+', logo: 'https://image.tmdb.org/t/p/original/6uhKBfmtzFqOcLousHwZuzcrjhK.jpg' }
+  { id: 8, name: 'Netflix', logo: 'https://image.tmdb.org/t/p/original/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg' },
+  { id: 119, name: 'Prime Video', logo: 'https://image.tmdb.org/t/p/original/pvske1MyAoymrs5bguRfVqYiM9a.jpg' },
+  { id: 337, name: 'Disney+', logo: 'https://image.tmdb.org/t/p/original/97yvRBw1GzX7fXprcF80er19ot.jpg' },
+  { id: 1899, name: 'Max', logo: 'https://image.tmdb.org/t/p/original/jbe4gVSfRlbPTdESXhEKpornsfu.jpg' },
+  { id: 350, name: 'Apple TV+', logo: 'https://image.tmdb.org/t/p/original/mcbz1LgtErU9p4UdbZ0rG6RTWHX.jpg' }
 ];
 
 export default function ProviderBar({ selectedProvider, onSelectProvider }) {
